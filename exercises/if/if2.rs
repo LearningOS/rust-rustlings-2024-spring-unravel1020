57306e2c33e24e4fdf7a96e717e85d7371da6ba9
Ramar Crown <1551751306@qq.com>
2024-04-08 14:09:14 +0000

    1

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -5,13 +5,14 @@
 //
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub fn foo_if_fizz(fizzish: &str) -> &str {
+pub fn foo_if_fizz(fizzish: &str) -> &'static str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
+    } else if fizzish == "fuzz"{
+        "bar"
+    }else{
+        "baz"
     }
 }
 
@@ -32,6 +33,6 @@ mod tests {
 
     #[test]
     fn default_to_baz() {
-        assert_eq!(foo_if_fizz("literally anything"), "baz")
+        assert_eq!(foo_if_fizz("literally anything "), "baz")
     }
 }
