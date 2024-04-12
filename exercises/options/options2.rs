81539389359196be561c51757091e2373b85a27d
Ramar Crown <1551751306@qq.com>
2024-04-12 17:31:25 +0000

    1

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -13,7 +11,7 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -32,11 +30,14 @@ mod tests {
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
-        integer = optional_integers.pop() {
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
-
+        // - The outer Option represents the possibility that the vector might be empty (None),  
+        //   or that an element was successfully popped (Some).  
+        // - The inner Option represents the actual value stored in the vector, which might be  
+        //   Some(i8) if there is a value, or None if the vector contained a None element.  
         assert_eq!(cursor, 0);
     }
 }
